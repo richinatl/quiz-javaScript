0784bfe79f636d9cdb580f438e6d85b151fb7ec3
Richard Murphy <richinatl@gmail.com>
2022-01-31 04:17:16 -0500

    Added html, css, and javascript code

---
+++ script.js
@@ -0,0 +1,172 @@
+(function () {
+  function makeQuiz() {
+    var output = [];
+    quizQuestions.forEach(function (currentQuestion, questionNumber) {
+      var answers = [];
+
+      for (letter in currentQuestion.answers) {
+        answers.push(
+          '<label>\n <input type="radio" name="question'
+            .concat(questionNumber, '" value="')
+            .concat(letter, '">\n')
+            .concat(letter, " :\n")
+            .concat(currentQuestion.answers[letter], "\n </label>")
+        );
+      }
+
+      output.push(
+        '<div class="slide">\n <div class="question"> '
+          .concat(currentQuestion.question, ' </div>\n<div class="answers"> ')
+          .concat(answers.join(""), " </div>\n -</div>")
+      );
+    });
+    quizContainer.innerHTML = output.join("");
+  }
+
+  function showResults() {
+    var answerContainers = quizContainer.querySelectorAll(".answers");
+    var numCorrect = 0;
+    quizQuestions.forEach(function (currentQuestion, questionNumber) {
+      var answerContainer = answerContainers[questionNumber];
+      var selector = "input[name=question".concat(questionNumber, "]:checked");
+      var userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+      if (userAnswer === currentQuestion.correctAnswer) {
+        numCorrect++;
+      }
+    });
+    resultsContainer.innerHTML = ""
+      .concat(numCorrect, " out of ")
+      .concat(quizQuestions.length);
+    localStorage.setItem("", numCorrect);
+    localStorage.getItem("", numCorrect);
+    alert("You got a score of " + numCorrect + "!");
+  }
+
+  function slideShow(n) {
+    slides[currentSlide].classList.remove("active--slide");
+    slides[n].classList.add("active--slide");
+    currentSlide = n;
+
+    if (currentSlide === 0) {
+      previousButton.style.display = "none";
+    } else {
+      previousButton.style.display = "inline-block";
+    }
+
+    if (currentSlide === slides.length - 1) {
+      nextButton.style.display = "none";
+      submitButton.style.display = "inline-block";
+    } else {
+      nextButton.style.display = "inline-block";
+      submitButton.style.display = "none";
+    }
+  }
+
+  function showNext() {
+    slideShow(currentSlide + 1);
+  }
+
+  function showPrevious() {
+    slideShow(currentSlide - 1);
+  }
+
+  var quizContainer = document.getElementById("quiz");
+  var resultsContainer = document.getElementById("results");
+  var submitButton = document.getElementById("submit");
+  var quizQuestions = [
+    {
+      question: "What is the first pillar of object oriented programming?",
+      answers: {
+        a: "Abstraction",
+        b: "Encapsulation",
+        c: "Polymorphism",
+        d: "Inheritance",
+      },
+      correctAnswer: "b",
+    },
+    {
+      question: "Hoisting is the process of moving ______",
+      answers: {
+        a: "Functions to the top of the file",
+        b: "Objects to the top of the file",
+        c: "Callback Functions",
+        d: "None of the above",
+      },
+      correctAnswer: "a",
+    },
+    {
+      question: "Which of these removes the first element of an array?",
+      answers: {
+        a: "pop",
+        b: "slice",
+        c: "unshift",
+        d: "shift",
+      },
+      correctAnswer: "d",
+    },
+    {
+      question: "Template literals use which punctuation marks",
+      answers: {
+        a: "single quotes",
+        b: "double quotes",
+        c: "backticks",
+        d: "backslashes",
+      },
+      correctAnswer: "c",
+    },
+    {
+      question:
+        "What naming convention is typically used with a constructor function?",
+      answers: {
+        a: "PascalCase",
+        b: "Camelcase",
+        c: "Snakecase",
+        d: "Hungarian notation",
+      },
+      correctAnswer: "a",
+    },
+    {
+      question: "What is used to iterate over the objects of an array?",
+      answers: {
+        a: "if and else",
+        b: "for",
+        c: "switch and case",
+        d: "for in",
+      },
+      correctAnswer: "d",
+    },
+    {
+      question: "What is a method",
+      answers: {
+        a: "a function in an object",
+        b: "a function that creates an object",
+        c: "a function that depends on another function",
+        d: "both a and b",
+      },
+      correctAnswer: "a",
+    },
+    {
+      question: "Which of these statements are true",
+      answers: {
+        a: "Primitives are copied by their value",
+        b: "Objects are copied by their reference",
+        c: "Both a and b",
+        d: "Neither a or b",
+      },
+      correctAnswer: "c",
+    },
+  ];
+  makeQuiz();
+
+  var previousButton = document.getElementById("previous");
+  var nextButton = document.getElementById("next");
+  var slides = document.querySelectorAll(".slide");
+  var currentSlide = 0;
+
+  slideShow(currentSlide);
+
+  submitButton.addEventListener("click", showResults);
+  previousButton.addEventListener("click", showPrevious);
+  nextButton.addEventListener("click", showNext);
+})();
